b182824f6b4f33fc4e114a4a173c8aea293d9b8e
ManrajSingh02 <singhmanraj6702@gmail.com>
2026-02-09 16:30:51 +0530

    link with git

---
+++ index.js
@@ -0,0 +1,34 @@
+bt1.addEventListener('click',function(){
+ alert('You are using browser');
+});
+
+
+document.getElementById('bt2').addEventListener('click',function(){
+ document.getElementById('para').innerText='text change';
+});
+
+
+
+document.getElementById('bt3').addEventListener('click',function(){
+ document.getElementById('container').style.backgroundColor='orange' ;
+});
+
+
+
+
+
+bt4.addEventListener('click',function(){
+ alert('Hello!, you are using javascript');
+});
+
+
+
+const firstname = 'Manraj';
+const lastname = 'singh';
+const age=23;
+const address='phagwara';
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+console

3bbc336d39bd6be54578cf198e5cce95058c81ce
ManrajSingh02 <singhmanraj6702@gmail.com>
2026-02-15 17:30:40 +0530

    UI Interface

--- UI interface with conditional user input/checker.js
+++ UI interface with conditional user input/checker.js
@@ -1,11 +1,15 @@
-    function detalis(){
-       
+  console.log("Button clicked");
+document.getElementById("bt").addEventListener("click", detalis);
   
+function detalis(event){
+  event.preventDefault();
+  
+
     const enterName = document.getElementById("name").value;
       const enterAge = document.getElementById("age").value;
-      const enterMathMarks =(document.getElementById("math").value);
-    const  enterEnglishMarks =(document.getElementById("english").value);
-    const enterScienceMarks =(document.getElementById("science").value)
+      const enterMathMarks =Number(document.getElementById("math").value);
+    const  enterEnglishMarks =Number(document.getElementById("english").value);
+    const enterScienceMarks =Number(document.getElementById("science").value)
 
       const stu = {
         name: enterName,
@@ -16,18 +20,18 @@
             science: enterScienceMarks,
         }
       };
-
-const total = student.marks.math + 
-                  student.marks.english + 
-                  student.marks.science;
-
+console.log("stu object created");
+const total = stu.marks.math + 
+                  stu.marks.english + 
+                  stu.marks.science;
+console.log("total object created");
     const average = total / 3;
-
+console.log("average object created");
     
 
       let message = "";
 
-
+console.log("let variable created");
 
       if (stu.age < 18) {
         message = "You must be 18+ to pass";
@@ -39,7 +43,7 @@ const total = student.marks.math +
         document.body.style.backgroundColor = "green";
     } 
     else if (average >= 40) {
-        message = "Pass";
+        message = "Pss";
         document.body.style.backgroundColor = "orange";
     } 
       
@@ -57,6 +61,6 @@ const total = student.marks.math +
 
       document.getElementById("messageOutput").textContent =
         "Result: " + message;
-    student.name = student.name.toUpperCase(); 
-     student = {}; 
+   
+    
      }
